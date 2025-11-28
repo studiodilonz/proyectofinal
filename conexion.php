0364e67c1e34931d2dac4ab42c73df37b3f5b58e
studiodilonz <studiodilonz@gmail.com>
2025-11-27 23:38:31 -0400

    en trabajo formal_db

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "mipagina_db");
+$conexion = new mysqli("localhost", "root", "", "multiservicios_contactos");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
